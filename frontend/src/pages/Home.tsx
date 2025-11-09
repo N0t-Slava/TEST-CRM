565ba9bf4833b8084a6ff09aaff8b8933c83339c
Slava <nepejcevslava@gmail.com>
2025-11-09 17:40:59 +0200

    feat(my_app): initialize frontend
    <body - зачем изменение, детали>
    
    <footer - ссылки на issue/теги>

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import Header from "../../components/header";
+import Hero from "../../components/hero";
+import Footer from "../../components/footer";
+
+export default function Home(){
+    return (
+    <main className="bg-background min-h-screen">
+        <Header />
+        <Hero />
+        <Footer /> 
+    </main>);
+}
